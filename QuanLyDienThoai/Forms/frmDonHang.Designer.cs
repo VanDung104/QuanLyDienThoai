b8eb7642360a043d2c47b4235728ce32ab73ee9a
Chu Dung <vandung1042003@gmail.com>
2023-11-03 16:15:14 +0700

    update

--- QuanLyDienThoai/Forms/frmDonHang.Designer.cs
+++ QuanLyDienThoai/Forms/frmDonHang.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(330, 13);
+            this.label1.Location = new System.Drawing.Point(460, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 16);
             this.label1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1108, 557);
             this.Controls.Add(this.label1);
             this.Name = "frmDonHang";
             this.Text = "frmDonHang";
